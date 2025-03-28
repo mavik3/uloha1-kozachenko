654245fcce89690e1fe8be52b3b58b1b5d00fe6f
mavik3 <sasakozacenko885@gmail.com>
2025-03-28 16:38:28 +0100

    finish

--- uloha1.c
+++ uloha1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-	unsigned int nasobky_v_rozsahu(unsigned int m, int a,b){
+	unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b){
 		
-	return (b/m-(a-1)/m);
+	return ( b / m - ( a - 1 ) / m);
 	
 	}
 	
 	int main (){
-	int a,b;
+	int a, b;
 	unsigned int m;
 	
-	scanf("%d",&m);
-	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %d ",m,a,b,nasobky_v_rozsahu(m,a,b));
+	scanf("%d %d %d",&m,&a,&b);
+	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
 			}
 	
 	
