ee1467fdde2509bf3d2399bcbd0b562d76121e6d
mavik3 <sasakozacenko885@gmail.com>
2025-03-28 16:31:40 +0100

    zacatok ale je chyba

--- uloha1.c
+++ uloha1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+	unsigned int nasobky_v_rozsahu(unsigned int m, int a,b){
+		
+	return (b/m-(a-1)/m);
 	
-	int main (void)
-		{
-			
-		}
+	}
+	
+	int main (){
+	int a,b;
+	unsigned int m;
+	
+	scanf("%d",&m);
+	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %d ",m,a,b,nasobky_v_rozsahu(m,a,b));
+			}
 	
 	
