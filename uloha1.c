d6878806f6c5511a1b469087a9dc0bb95396543d
mavik3 <sasakozacenko885@gmail.com>
2025-04-01 12:56:34 +0200

    finish2 urobil aby code vyzeral lepse

--- uloha1.c
+++ uloha1.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-	unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b){
-		if(a==0 || b==0 || m==0)return 0;
-	return ( b / m - ( a - 1 ) / m);
 
+	unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b)
+		{
+			if(a==0 || b==0 || m==0)return 0;
+			return ( b / m - ( a - 1 ) / m);	
+		}
 	
-	}
-	
-	int main (){
-	int a, b;
-	unsigned int m;
-	
-	scanf("%d %d %d",&m,&a,&b);
-	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
-			}
+	int main ()
+		{
+			int a, b;
+			unsigned int m;
+			
+			scanf("%d %d %d",&m,&a,&b);
+			printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
+
+			return 0;
+		}
 	
 	
