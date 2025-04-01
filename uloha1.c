b390cf9fdc44921f95d9e0d0cee098e82e39fbac
Peter Sarkoci <petak@dolichocephalus>
2025-04-01 16:17:16 +0200

    odsadzanie takto

--- uloha1.c
+++ uloha1.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-	unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b)
-		{
-			if(a==0 || b==0 || m==0)return 0;
-			return ( b / m - ( a - 1 ) / m);	
-		}
-	
-	int main ()
-		{
-			int a, b;
-			unsigned int m;
-			
-			scanf("%d %d %d",&m,&a,&b);
-			printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
+unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b)
+{
+	if(a==0 || b==0 || m==0)return 0;
+	return ( b / m - ( a - 1 ) / m);	
+}
 
-			return 0;
-		}
+int main ()
+{
+	int a, b;
+	unsigned int m;
 	
-	
+	scanf("%d %d %d",&m,&a,&b);
+	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
+
+	return 0;
+}
