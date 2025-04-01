5dfd0d589ad4d8bdcb73d679113bfe2bdacacb38
Peter Sarkoci <petak@dolichocephalus>
2025-04-01 16:31:58 +0200

    este upravy zdrojaku

--- uloha1.c
+++ uloha1.c
@@ -2,17 +2,19 @@
 
 unsigned int nasobky_v_rozsahu(unsigned int m, int a, int b)
 {
-	if(a==0 || b==0 || m==0)return 0;
-	return ( b / m - ( a - 1 ) / m);	
+	if(a==0 || b==0 || m==0)
+        return 0;
+
+	return( b / m - ( a - 1 ) / m);	
 }
 
-int main ()
+int main(void)
 {
 	int a, b;
 	unsigned int m;
 	
 	scanf("%d %d %d",&m,&a,&b);
-	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ",m,a,b,nasobky_v_rozsahu(m,a,b));
+	printf("kolko nasobkov kratnych %d na intervale [%d,%d]: %u ", m, a, b, nasobky_v_rozsahu(m,a,b));
 
 	return 0;
 }
